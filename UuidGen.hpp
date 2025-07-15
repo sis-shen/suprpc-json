45e0df086009000d712e1bbfaeb4e6074bd75676
sis-shen <adongfa@gmail.com>
2025-07-15 22:19:50 +0800

    新增uuid生成

---
+++ UuidGen.hpp
@@ -0,0 +1,16 @@
+/**
+ * @file UuidGen.hpp
+ * @author supdriver
+ * @date 2025-05-26
+ * @brief uuid的生成
+ * @version 2.6.0
+ */
+
+#include <iostream>
+#include <chrono>
+#include <random>
+#include <string>
+#include <sstream>
+#include <atomic>
+#include <iomanip>
+
