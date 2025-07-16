1d16280e15614db4c8af11831363da1a2a29890e
sis-shen <adongfa@gmail.com>
2025-07-16 22:05:22 +0800

    完成uuid生成

--- UuidGen.hpp
+++ UuidGen.hpp
@@ -14,3 +14,31 @@
 #include <atomic>
 #include <iomanip>
 
+/**
+ * @brief 生成uuid
+ */
+
+static std::string uuid()
+{
+   std::stringstream ss;
+   // 构造一个机器随机数对象
+   std::random_device rd;
+   std::mt19937 generator(rd());
+   std::uniform_int_distribution<int> distribution(0, 255);
+   for (int i = 0; i < 8; i++)
+   {
+      if (i == 4 || i == 6)
+         ss << "-";
+      ss << std::setw(2) << std::setfill('0') << std::hex << distribution(generator);
+   }
+   ss << "-";
+   static std::atomic<size_t> seq(1);
+   size_t cur = seq.fetch_add(1);
+   for (int i = 7; i > 0; i--)
+   {
+      if (i == 5)
+         ss << "-";
+      ss << std::setw(2) << std::setfill('0') << std::hex << ((cur >> (i * 8)) & 0xFF);
+   }
+   return ss.str();
+}
