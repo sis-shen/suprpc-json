ad9acbb2fefb7088d6d367d18dca748269047050
sis-shen <adongfa@gmail.com>
2025-07-31 16:06:58 +0800

    完成对消息类的封装

--- Message.hpp
+++ Message.hpp
@@ -159,5 +159,133 @@
             val[KEY_HOST_PORT] = addr.second;
             _body[KEY_HOST] = val;
         }
-    }
+
+    };
+
+    /**
+     * @class RpcResponse
+     * @brief RPC响应类的实现
+     */
+    class RpcResponse:public JsonResponse{
+        using ptr = std::shared_ptr<RpcResponse>;
+        virtual bool check()override{
+            if(_body[KEY_RCODE].isNull() ==true ||
+                _body[KEY_RCODE].isIntegral() == false){
+                    SUP_LOG_ERROR("RPC响应中无状态码或者状态码类型错误！");
+                    return false;
+                }
+            if(_body[KEY_RESULT].isNull() == true ||
+                _body[KEY_RESULT].isObject() == false){
+                SUP_LOG_ERROR("RPC响应中无Rpc调用结果，或者结果类型错误！");
+                return false;
+            }
+        }
+
+        Json::Value result(){
+            return _body[KEY_RESULT];
+        }
+
+        void setResult(const Json::Value &result){
+            _body[KEY_RESULT] = result;
+        }
+    };
+
+    /**
+     * @class TopicResponse
+     * @brief 话题响应类
+     */
+    class TopicResponse:public JsonResponse{
+        public:
+        using ptr = std::shared_ptr<TopicResponse>;
+    };
+
+    /**
+     * @class ServiceResponse
+     * @brief 服务响应类
+     */
+    class ServiceResponse:public JsonResponse{
+        public:
+        using ptr = std::shared_ptr<ServiceResponse>;
+        virtual bool check() override{
+            if(_body[KEY_RCODE].isNull() ==true ||
+            _body[KEY_RCODE].isIntegral() == false){
+                SUP_LOG_ERROR("服务响应中无状态码或者状态码类型错误！");
+                return false;
+            }
+            if(_body[KEY_OPTYPE].isNull() == true ||
+                _body[KEY_OPTYPE].isIntegral() == false){
+                    SUP_LOG_ERROR("服务响应中没有操作类型，或者操作类型的类型错误！");
+                    return false;
+                }
+            if(_body[KEY_OPTYPE].asInt() == (int)ServiceOptype::SERVICE_DISCOVERY&&
+                (_body[KEY_METHOD].isNull() == true ||
+                _body[KEY_METHOD].isString() == false ||
+                _body[KEY_HOST].isNull() == true ||
+                _body[KEY_HOST].isArray() == false)){
+                    SUP_LOG_ERROR("服务发现响应中响应信息字段错误！");
+                    return false;
+                }
+
+                return true;
+        }
+        ServiceOptype optype(){
+            return (ServiceOptype)_body[KEY_OPTYPE].asInt();
+        }
+        void setOptype(const ServiceOptype optype){
+            _body[KEY_OPTYPE] = (int)optype;
+        }
+        std::string method(){
+            return _body[KEY_METHOD].asString();
+        }
+
+        void setMethod(const std::string &method){
+            _body[KEY_METHOD] = method;
+        }
+
+        std::vector<Address> hosts(){
+            std::vector<Address> addrs;
+            int sz = _body[KEY_HOST].size();
+            for(int i = 0;i<sz;++i){
+                Address addr;
+                addr.first = _body[KEY_HOST][KEY_HOST_IP].asString();
+                addr.second = _body[KEY_HOST][KEY_HOST_PORT].asInt();
+                addrs.push_back(addr);
+            }
+            return addrs;
+        }
+
+        void setHosts(const std::vector<Address>&addrs){
+            for(auto&addr:addrs){
+                Json::Value val;
+                val[KEY_HOST_IP] = addr.first;
+                val[KEY_HOST_PORT] = addr.second;
+                _body[KEY_HOST].append(val);
+
+            }
+        }
+    };
+
+    /**
+     * @class MessageFactory
+     * @brief 生成消息对象的工厂
+     */
+    class MessageFactory{
+        public:
+        static BaseMessage::ptr create(const MType mtype){
+            switch(mtype){
+                case MType::REQ_RPC:return std::make_shared<RpcRequest>();
+                case MType::RSP_RPC:return std::make_shared<RpcResponse>();
+                case MType::REQ_TOPIC:return std::make_shared<TopicRequest>();
+                case MType::RSP_TOPIC:return std::make_shared<TopicResponse>();
+                case MType::REQ_SERVICE:return std::make_shared<ServiceRequest>();
+                case MType::RSP_SERVICE:return std::make_shared<ServiceResponse>();
+            }
+            return std::make_shared<BaseMessage>();
+        }
+
+        template<class T,class ...Args>
+        static BaseMessage::ptr create(Args&& ...args){
+            return std::make_shared<T>(std::forward(args)...);
+        }
+    };
  }
