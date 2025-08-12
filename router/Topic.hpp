be3df57b47ce19033414942d8c1151e339dc1cac
sis-shen <adongfa@gmail.com>
2025-08-12 17:28:26 +0800

    新增TopicManager类

--- router/Topic.hpp
+++ router/Topic.hpp
@@ -2,3 +2,40 @@
  * @file Topic.hpp
  * @brief 主题的发布和订阅的实现
  */
+
+#pragma once
+#include "../common/MuduoTool.hpp"
+#include "../common/Message.hpp"
+
+#include <unordered_map>
+#include <unordered_set>
+
+namespace suprpc{
+    namespace server{
+        /**
+         * @class TopicManager
+         * @brief 话题管理类
+         */
+        class TopicManager{
+            public:
+            using ptr = std::shared_ptr<TopicManager>;
+            TopicManager(){}
+            void onTopicRequest(const BaseConnection::ptr &conn,
+                const TopicRequest::ptr& msg){
+                    TopicOptype topic_optype = msg->optype();
+                    bool ret = true;
+                    switch (topic_optype)
+                    {
+                    case TopicOptype::TOPIC_CRAETE :
+                        /* code */
+                        break;
+                    
+                    default:
+                        break;
+                    }
+                }
+
+        
+        };
+}
+}
