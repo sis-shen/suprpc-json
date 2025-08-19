8394c5db786ad1fb3b7cc741f771e3b2ec67f709
sis-shen <adongfa@gmail.com>
2025-08-19 15:22:57 +0800

    话题管理相关代码封装完成

--- router/Topic.hpp
+++ router/Topic.hpp
@@ -10,36 +10,43 @@
 #include <unordered_map>
 #include <unordered_set>
 
-namespace suprpc{
-    namespace server{
+namespace suprpc
+{
+    namespace server
+    {
         /**
          * @class TopicManager
          * @brief 话题管理类
          */
-        class TopicManager{
-            public:
+        class TopicManager
+        {
+        public:
             using ptr = std::shared_ptr<TopicManager>;
-            TopicManager(){}
+            TopicManager() {}
             void onTopicRequest(const BaseConnection::ptr &conn,
-                const TopicRequest::ptr& msg){
-                    TopicOptype topic_optype = msg->optype();
-                    bool ret = true;
-                    switch (topic_optype)
-                    {
-                    case TopicOptype::TOPIC_CRAETE :
-                        /* code */
-                        break;
-                    
-                    default:
-                        break;
-                    }
-                }
+                                const TopicRequest::ptr &msg)
+            {
+                TopicOptype topic_optype = msg->optype();
+                bool ret = true;
+                switch (topic_optype)
+                {
+                case TopicOptype::TOPIC_CRAETE:
+                    /* code */
+                    break;
 
-            void onShutDown(const BaseConnection::ptr &conn){
-                // std::vector<Topic::ptr> 
+                default:
+                    break;
+                }
             }
+
+            void onShutDown(const BaseConnection::ptr &conn)
+            {
+                // std::vector<Topic::ptr>
+            }
+
         private:
-            void errorResponse(const BaseConnection::ptr &conn,const TopicRequest::ptr &msg,RCode code){
+            void errorResponse(const BaseConnection::ptr &conn, const TopicRequest::ptr &msg, RCode code)
+            {
                 auto msg_rsp = MessageFactory::create<TopicResponse>();
                 msg_rsp->setId(msg->rid());
                 msg_rsp->setMType(MType::RSP_TOPIC);
@@ -47,7 +54,8 @@ namespace suprpc{
                 conn->send(msg_rsp);
             }
 
-            void topicResponse(const BaseConnection::ptr &conn,const TopicRequest::ptr &msg ){
+            void topicResponse(const BaseConnection::ptr &conn, const TopicRequest::ptr &msg)
+            {
                 auto msg_rsp = MessageFactory::create<TopicResponse>();
                 msg_rsp->setId(msg->rid());
                 msg_rsp->setMType(MType::RSP_TOPIC);
@@ -55,85 +63,158 @@ namespace suprpc{
                 conn->send(msg_rsp);
             }
 
-            void topicRemove(const BaseConnection::ptr &conn,const TopicRequest::ptr &msg){
+            void topicRemove(const BaseConnection::ptr &conn, const TopicRequest::ptr &msg)
+            {
                 std::string topic_name = msg->topicKey();
                 std::unordered_set<Subscriber::ptr> subscribers;
                 {
                     std::unique_lock<std::mutex> lock(_mutex);
                     auto it = _topics.find(topic_name);
-                    if(it == _topics.end()){
-                        return ;
+                    if (it == _topics.end())
+                    {
+                        return;
                     }
 
                     subscribers = it->second->_subscribers;
-                    _topics.erase(it);//删除当前主题的映射关系
+                    _topics.erase(it); // 删除当前主题的映射关系
                 }
-                for(auto &subscriber:subscribers){
+                for (auto &subscriber : subscribers)
+                {
                     subscriber->removeTopic(topic_name);
                 }
             }
 
-            bool topicSubscriber(const BaseConnection::ptr & conn,const TopicRequest::ptr &msg){
+            bool topicSubscriber(const BaseConnection::ptr &conn, const TopicRequest::ptr &msg)
+            {
                 Topic::ptr topic;
                 Subscriber::ptr subscriber;
                 {
-                    std::unique_lock<std::mutex>lock(_mutex);
+                    std::unique_lock<std::mutex> lock(_mutex);
                     auto topic_it = _topics.find(msg->topicKey());
-                    if(topic_it == _topics.end()){
+                    if (topic_it == _topics.end())
+                    {
                         return false;
                     }
                     topic = topic_it->second;
-                    
-
+                    auto sub_it = _subscribers.find(conn);
+                    if (sub_it != _subscribers.end())
+                    {
+                        subscriber = sub_it->second;
+                    }
+                    else
+                    {
+                        subscriber = std::make_shared<Subscriber>(conn);
+                        _subscribers.insert(std::make_pair(conn, subscriber));
+                    }
                 }
+
+                topic->appendSubscriber(subscriber);
+                subscriber->appendTopic(msg->topicKey());
+            }
+
+            void topicCcancel(const BaseConnection::ptr &conn, const TopicRequest::ptr &msg)
+            {
+                Topic::ptr topic;
+                Subscriber::ptr subscriber;
+                {
+                    std::unique_lock<std::mutex> lock(_mutex);
+                    auto topic_it = _topics.find(msg->topicKey());
+                    if (topic_it != _topics.end())
+                    {
+                        topic = topic_it->second;
+                    }
+                    auto sub_it = _subscribers.find(conn);
+                    if (sub_it != _subscribers.end())
+                    {
+                        subscriber = sub_it->second;
+                    }
+                }
+                if (subscriber)
+                    subscriber->removeTopic(msg->topicKey());
+                if (topic && subscriber)
+                    topic->removeSubscriber(subscriber);
+            }
+
+            bool topicPublish(const BaseConnection::ptr &conn, const TopicRequest::ptr &msg)
+            {
+                Topic::ptr topic;
+                {
+                    std::unique_lock<std::mutex> lock(_mutex);
+                    auto topic_it = _topics.find(msg->topicKey());
+                    if (topic_it == _topics.end())
+                    {
+                        return false;
+                    }
+                    topic = topic_it->second;
+                }
+                topic->pushMessage(msg);
+                return true;
             }
 
         private:
-            struct Subscriber{
+            /**
+             * @class Subscriber
+             * @brief 订阅者类
+             */
+            struct Subscriber
+            {
                 using ptr = std::shared_ptr<Subscriber>;
                 std::mutex _mutex;
                 BaseConnection::ptr _conn;
                 std::unordered_set<std::string> _topics;
 
                 Subscriber(const BaseConnection::ptr &conn)
-                    :_conn(conn)
-                    {}
-                void appendTopic(const std::string&topic_name){
+                    : _conn(conn)
+                {
+                }
+                void appendTopic(const std::string &topic_name)
+                {
                     std::unique_lock<std::mutex> lock(_mutex);
                     _topics.insert(topic_name);
                 }
 
-                void removeTopic(const std::string& topic_name){
+                void removeTopic(const std::string &topic_name)
+                {
                     std::unique_lock(topic_name);
                     _topics.erase(topic_name);
                 }
             };
-            struct Topic{
+            /**
+             * @class Topic
+             * @brief 话题类
+             */
+            struct Topic
+            {
                 using ptr = std::shared_ptr<Topic>;
                 std::mutex _mutex;
                 std::string _topic_name;
-                
+
                 std::unordered_set<Subscriber::ptr> _subscribers;
-                Topic(const std::string &name): _topic_name(name) {}
-                void appendSubscriber(const Subscriber::ptr&subscriber){
+                Topic(const std::string &name) : _topic_name(name) {}
+                void appendSubscriber(const Subscriber::ptr &subscriber)
+                {
                     std::unique_lock<std::mutex> lock(_mutex);
                     _subscribers.insert(subscriber);
                 }
-                void removeSubscriber(const Subscriber::ptr &subscriber){
+                void removeSubscriber(const Subscriber::ptr &subscriber)
+                {
                     std::unique_lock<std::mutex> lock(_mutex);
                     _subscribers.erase(subscriber);
                 }
-                void pushMessage(const BaseMessage::ptr &msg){
+                void pushMessage(const BaseMessage::ptr &msg)
+                {
                     std::unique_lock<std::mutex> lock(_mutex);
-                    for(auto&subscriber : _subscribers){
+                    for (auto &subscriber : _subscribers)
+                    {
                         subscriber->_conn->send(msg);
                     }
                 }
             };
-            private:
+
+        private:
             std::mutex _mutex;
-            std::unordered_map<std::string,Topic::ptr> _topics;
-            std::unordered_map<std::string,Subscriber::ptr> _subscribers;
+            std::unordered_map<std::string, Topic::ptr> _topics;
+            std::unordered_map<BaseConnection::ptr, Subscriber::ptr> _subscribers;
         };
-}
+    }
 }
